c90eff118d374e374768a0fe916cde0fdf5cef86
Gaeun0706 <asdkjfgh@naver.com>
2025-04-03 17:40:20 +0900

    Add files via upload

---
+++ 012_ComboBox_grade/012_ComboBox_grade/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace _012_ComboBox_grade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt7 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.crd1 = new System.Windows.Forms.ComboBox();
+            this.crd5 = new System.Windows.Forms.ComboBox();
+            this.crd4 = new System.Windows.Forms.ComboBox();
+            this.crd6 = new System.Windows.Forms.ComboBox();
+            this.crd7 = new System.Windows.Forms.ComboBox();
+            this.crd3 = new System.Windows.Forms.ComboBox();
+            this.crd2 = new System.Windows.Forms.ComboBox();
+            this.grd2 = new System.Windows.Forms.ComboBox();
+            this.grd3 = new System.Windows.Forms.ComboBox();
+            this.grd7 = new System.Windows.Forms.ComboBox();
+            this.grd6 = new System.Windows.Forms.ComboBox();
+            this.grd5 = new System.Windows.Forms.ComboBox();
+            this.grd4 = new System.Windows.Forms.ComboBox();
+            this.grd1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtGrade = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(156, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "과목명";
+            // 
+            // txt2
+            // 
+            this.txt2.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txt2.Location = new System.Drawing.Point(92, 105);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(182, 30);
+            this.txt2.TabIndex = 1;
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txt1.Location = new System.Drawing.Point(92, 65);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(182, 30);
+            this.txt1.TabIndex = 3;
+            // 
+            // txt7
+            // 
+            this.txt7.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txt7.Location = new System.Drawing.Point(92, 344);
+            this.txt7.Name = "txt7";
+            this.txt7.Size = new System.Drawing.Size(182, 30);
+            this.txt7.TabIndex = 4;
+            // 
+            // txt6
+            // 
+            this.txt6.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txt6.Location = new System.Drawing.Point(92, 297);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(182, 30);
+            this.txt6.TabIndex = 5;
+            // 
+            // txt5
+            // 
+            this.txt5.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txt5.Location = new System.Drawing.Point(92, 254);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(182, 30);
+            this.txt5.TabIndex = 6;
+            // 
+            // txt3
+            // 
+            this.txt3.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txt3.Location = new System.Drawing.Point(92, 154);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(182, 30);
+            this.txt3.TabIndex = 7;
+            // 
+            // txt4
+            // 
+            this.txt4.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txt4.Location = new System.Drawing.Point(92, 207);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(182, 30);
+            this.txt4.TabIndex = 8;
+            // 
+            // crd1
+            // 
+            this.crd1.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.crd1.FormattingEnabled = true;
+            this.crd1.Location = new System.Drawing.Point(303, 66);
+            this.crd1.Name = "crd1";
+            this.crd1.Size = new System.Drawing.Size(121, 31);
+            this.crd1.TabIndex = 9;
+            // 
+            // crd5
+            // 
+            this.crd5.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.crd5.FormattingEnabled = true;
+            this.crd5.Location = new System.Drawing.Point(303, 240);
+            this.crd5.Name = "crd5";
+            this.crd5.Size = new System.Drawing.Size(121, 31);
+            this.crd5.TabIndex = 10;
+            // 
+            // crd4
+            // 
+            this.crd4.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.crd4.FormattingEnabled = true;
+            this.crd4.Location = new System.Drawing.Point(303, 198);
+            this.crd4.Name = "crd4";
+            this.crd4.Size = new System.Drawing.Size(121, 31);
+            this.crd4.TabIndex = 11;
+            // 
+            // crd6
+            // 
+            this.crd6.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.crd6.FormattingEnabled = true;
+            this.crd6.Location = new System.Drawing.Point(303, 279);
+            this.crd6.Name = "crd6";
+            this.crd6.Size = new System.Drawing.Size(121, 31);
+            this.crd6.TabIndex = 12;
+            // 
+            // crd7
+            // 
+            this.crd7.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.crd7.FormattingEnabled = true;
+            this.crd7.Location = new System.Drawing.Point(303, 321);
+            this.crd7.Name = "crd7";
+            this.crd7.Size = new System.Drawing.Size(121, 31);
+            this.crd7.TabIndex = 13;
+            // 
+            // crd3
+            // 
+            this.crd3.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.crd3.FormattingEnabled = true;
+            this.crd3.Location = new System.Drawing.Point(303, 154);
+            this.crd3.Name = "crd3";
+            this.crd3.Size = new System.Drawing.Size(121, 31);
+            this.crd3.TabIndex = 14;
+            // 
+            // crd2
+            // 
+            this.crd2.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.crd2.FormattingEnabled = true;
+            this.crd2.Location = new System.Drawing.Point(303, 107);
+            this.crd2.Name = "crd2";
+            this.crd2.Size = new System.Drawing.Size(121, 31);
+            this.crd2.TabIndex = 15;
+            // 
+            // grd2
+            // 
+            this.grd2.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.grd2.FormattingEnabled = true;
+            this.grd2.Location = new System.Drawing.Point(452, 107);
+            this.grd2.Name = "grd2";
+            this.grd2.Size = new System.Drawing.Size(121, 31);
+            this.grd2.TabIndex = 22;
+            // 
+            // grd3
+            // 
+            this.grd3.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.grd3.FormattingEnabled = true;
+            this.grd3.Location = new System.Drawing.Point(452, 154);
+            this.grd3.Name = "grd3";
+            this.grd3.Size = new System.Drawing.Size(121, 31);
+            this.grd3.TabIndex = 21;
+            // 
+            // grd7
+            // 
+            this.grd7.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.grd7.FormattingEnabled = true;
+            this.grd7.Location = new System.Drawing.Point(452, 321);
+            this.grd7.Name = "grd7";
+            this.grd7.Size = new System.Drawing.Size(121, 31);
+            this.grd7.TabIndex = 20;
+            // 
+            // grd6
+            // 
+            this.grd6.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.grd6.FormattingEnabled = true;
+            this.grd6.Location = new System.Drawing.Point(452, 279);
+            this.grd6.Name = "grd6";
+            this.grd6.Size = new System.Drawing.Size(121, 31);
+            this.grd6.TabIndex = 19;
+            // 
+            // grd5
+            // 
+            this.grd5.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.grd5.FormattingEnabled = true;
+            this.grd5.Location = new System.Drawing.Point(452, 240);
+            this.grd5.Name = "grd5";
+            this.grd5.Size = new System.Drawing.Size(121, 31);
+            this.grd5.TabIndex = 18;
+            // 
+            // grd4
+            // 
+            this.grd4.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.grd4.FormattingEnabled = true;
+            this.grd4.Location = new System.Drawing.Point(452, 198);
+            this.grd4.Name = "grd4";
+            this.grd4.Size = new System.Drawing.Size(121, 31);
+            this.grd4.TabIndex = 17;
+            // 
+            // grd1
+            // 
+            this.grd1.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.grd1.FormattingEnabled = true;
+            this.grd1.Location = new System.Drawing.Point(452, 66);
+            this.grd1.Name = "grd1";
+            this.grd1.Size = new System.Drawing.Size(121, 31);
+            this.grd1.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(303, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 33);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "평균평점";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtGrade.Location = new System.Drawing.Point(452, 372);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.Size = new System.Drawing.Size(121, 30);
+            this.txtGrade.TabIndex = 24;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(337, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 23);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "학점";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(485, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 23);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "성적";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grd2);
+            this.Controls.Add(this.grd3);
+            this.Controls.Add(this.grd7);
+            this.Controls.Add(this.grd6);
+            this.Controls.Add(this.grd5);
+            this.Controls.Add(this.grd4);
+            this.Controls.Add(this.grd1);
+            this.Controls.Add(this.crd2);
+            this.Controls.Add(this.crd3);
+            this.Controls.Add(this.crd7);
+            this.Controls.Add(this.crd6);
+            this.Controls.Add(this.crd4);
+            this.Controls.Add(this.crd5);
+            this.Controls.Add(this.crd1);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt7);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "학점계산기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt7;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.ComboBox crd1;
+        private System.Windows.Forms.ComboBox crd5;
+        private System.Windows.Forms.ComboBox crd4;
+        private System.Windows.Forms.ComboBox crd6;
+        private System.Windows.Forms.ComboBox crd7;
+        private System.Windows.Forms.ComboBox crd3;
+        private System.Windows.Forms.ComboBox crd2;
+        private System.Windows.Forms.ComboBox grd2;
+        private System.Windows.Forms.ComboBox grd3;
+        private System.Windows.Forms.ComboBox grd7;
+        private System.Windows.Forms.ComboBox grd6;
+        private System.Windows.Forms.ComboBox grd5;
+        private System.Windows.Forms.ComboBox grd4;
+        private System.Windows.Forms.ComboBox grd1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtGrade;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ 012_ComboBox_grade/012_ComboBox_grade/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _012_ComboBox_grade
+{
+    public partial class Form1 : Form
+    {
+        TextBox[] titles;
+        ComboBox[] crds;
+        ComboBox[] grds;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt1.Text = "인체의 구조와 기능";
+            txt2.Text = "일반수학1";
+            txt3.Text = "디지털공학및실험";
+            txt4.Text = "비주얼프로그래밍";
+            txt5.Text = "설계및프로젝트기본I";
+            txt6.Text = "데이터사이언스";
+
+            //crd1.Items.Add(1);
+            //crd1.Items.Add(2);
+            //crd1.Items.Add(3);
+            //crd1.Items.Add(4);
+            //crd1.Items.Add(5);
+
+            crds = new ComboBox[] { crd1, crd2, crd3, crd4, crd5, crd6, crd7 };
+            grds = new ComboBox[] { grd1, grd2, grd3, grd4, grd5, grd6, grd7 };
+            titles = new TextBox[] { txt1, txt2, txt3, txt4, txt5, txt6, txt7 };
+
+            int[] arrCredit = { 1, 2, 3, 4, 5 };
+            List<string> lstGrade = new List<string> { "A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F" };
+
+            foreach (ComboBox c in crds)
+            {
+                foreach (int i in arrCredit)
+                {
+                    c.Items.Add(i);
+                }
+                c.SelectedItem = 2;
+            }
+
+            foreach (ComboBox c in grds)
+            {
+                foreach (string s in lstGrade)
+                {
+                    c.Items.Add(s);
+                }
+                c.SelectedItem = 3;
+            }
+
+            //  c.Items.Add("A+");
+            //  c.Items.Add("A0");
+            //  c.Items.Add("B+");
+            //  c.Items.Add("B0");
+            //  c.Items.Add("C+");
+            //  c.Items.Add("C0");
+            //  c.Items.Add("D+");
+            //  c.Items.Add("D0");
+            //  c.Items.Add("F");
+
+            //  c.SelectedItem = "A+";
+
+            //foreach (ComboBox c in crds)
+            //{
+            //     c.Items.Add(1);
+            //     c.Items.Add(2);
+            //     c.Items.Add(3);
+            //     c.Items.Add(4);
+            //     c.Items.Add(5);
+            //}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double tScore = 0;
+            double tCredit = 0;
+            for (int i = 0; i < crds.Length; i++)
+            {
+                if (titles[i].Text != "")
+                {
+                    int crd = int.Parse(crds[i].SelectedItem.ToString());
+                    tCredit += crd;
+                    tScore += crd * GetScore(grds[i].SelectedItem.ToString());
+
+                }
+            }
+
+            txtGrade.Text = (tScore / tCredit).ToString("0.00");
+        }
+
+        private double GetScore(string v)
+        {
+            if (v == "A+") return 4.5;
+            else if (v == "A0") return 4.0;
+            else if (v == "B+") return 3.5;
+            else if (v == "B0") return 3.0;
+            else if (v == "C+") return 2.5;
+            else if (v == "C0") return 2.0;
+            else if (v == "D+") return 1.5;
+            else if (v == "D0") return 1.0;
+            else return 0;
+        }
+    }
+}
+
